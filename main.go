c29d868fb67a7cb900093c88a8a48b469c6e172f
voutasaurus <voutasaurus@gmail.com>
2017-09-25 00:54:32 -0700

    simplify

--- main.go
+++ main.go
@@ -14,56 +14,43 @@ func main() {
 	if len(os.Args) < 2 || len(os.Args) > 3 {
 		log.Fatal("Expected single argument, got: %d", len(os.Args)-1)
 	}
-	phrase := os.Args[1]
-	dd, err := domainify(phrase)
+	dd, err := domainify(strings.ToLower(os.Args[1]))
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 	if len(dd) == 0 {
-		log.Fatalf("No domains found for: %q", phrase)
+		log.Fatalf("No domains found for: %q", os.Args[1])
 	}
 	for _, d := range dd {
 		fmt.Println(d)
 	}
 }
 
-type statusError struct {
-	got  int
-	want int
-}
-
-func (s statusError) Error() string {
-	return fmt.Sprintf("unexpected status code, want: %d, got: %d", s.want, s.got)
-}
-
 func domainify(phrase string) (domains []string, err error) {
-	r := strings.NewReplacer(".", "", "*", "")
-	phrase = strings.ToLower(phrase)
 	res, err := http.Get("https://publicsuffix.org/list/public_suffix_list.dat")
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, statusError{got: res.StatusCode, want: 200}
+		return nil, fmt.Errorf("want status: %d, got: %d", 200, res.StatusCode)
 	}
-	var dd []string
 	s := bufio.NewScanner(res.Body)
 	for s.Scan() {
 		line := s.Text()
 		if len(line) == 0 || strings.HasPrefix(line, "//") {
 			continue
 		}
-		suffix := r.Replace(line)
+		suffix := strings.NewReplacer(".", "", "*", "").Replace(line)
 		if strings.HasSuffix(phrase, suffix) {
-			for strings.HasPrefix(line, "*.") {
+			if strings.HasPrefix(line, "*.") {
 				line = line[2:]
 			}
-			dd = append(dd, phrase[:len(phrase)-len(suffix)]+"."+line)
+			domains = append(domains, phrase[:len(phrase)-len(suffix)]+"."+line)
 		}
 	}
 	if err := s.Err(); err != nil {
 		return nil, err
 	}
-	return dd, nil
+	return domains, nil
 }
